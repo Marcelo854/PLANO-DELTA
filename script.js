ef17d5bba12bc482293175161a18442c139c393d
Marcelo854 <marcelolicco1@hotmail.com>
2024-12-14 15:34:53 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
 function openModal(type) {
-    const modal = document.getElementById('modal');
-    const modalText = document.getElementById('modal-text');
-    modal.style.display = 'block';
-
     if (type === 'consulta') {
-        modalText.textContent = 'Agende sua consulta preenchendo o formulário de contato!';
+        alert("Agende sua consulta preenchendo o formulário!");
     } else if (type === 'planos') {
-        modalText.textContent = 'Conheça nossos planos entrando em contato com nossa equipe.';
+        alert("Conheça nossos planos de saúde!");
     }
-}
-
-function closeModal() {
-    const modal = document.getElementById('modal');
-    modal.style.display = 'none';
 }
